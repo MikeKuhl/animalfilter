a494a7ffa23e7bc431baaaa4cd1c9af7a66e5a83
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-11-30 23:39:29 -0800

    added react bootstramp

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import jsonGallery from "../data/data.json";
 
-function HornedGallery() {
-  return (
-    <div>
-      {jsonGallery.map((image, i) => (
-        <div className='beasts' key={i}>
-          <img src={`${image.image_url}`} alt={image.description} key={i} />
-        </div>
-      ))}
-    </div>
-  );
-}
+export default class HornedGallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    };
+  }
 
-export default HornedGallery;
+  handleClick = () => {
+    this.setState({ favorites: this.state.favorites + 1 });
+  };
+  render() {
+    return (
+      <Container fluid='md'>
+        <Row>
+          <div>
+            <Col lg={true}>
+              {jsonGallery.map((image, i) => (
+                <div className='beasts' key={i}>
+                  <img
+                    src={`${image.image_url}`}
+                    alt={image.description}
+                    key={i}
+                  />
+                  <li>
+                    {image.title}
+                    <br />
+                    {image.description}
+                    <br />
+                    Number of horns {image.horns}
+                    <br />
+                    <p>
+                      {" "}
+                      This beast has been favorited {
+                        this.state.favorites
+                      } times{" "}
+                    </p>
+                    <button onClick={this.handleClick}> Favorites</button>
+                  </li>
+                </div>
+              ))}
+            </Col>
+          </div>
+        </Row>
+      </Container>
+    );
+  }
+}
