cfd56e08b8fc7fe0f37496798be854291d54a764
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-12-01 22:41:39 -0800

    added modals

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,52 +1,77 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import jsonGallery from "../data/data.json";
-
-export default class HornedGallery extends Component {
+import SelectedBeast from "./SelectedBeast";
+export default class HornedBeast extends Component {
   constructor(props) {
     super(props);
     this.state = {
       favorites: 0,
+      shouldShowModal: false,
+      modalImageSrc: "",
+      imageDescription: "",
+      title: "",
     };
   }
+
+  // reference https://linguinecode.com/post/how-to-pass-parameter-values-to-onclick-react-function
+  openModal = (title) => (props) => {
+    this.setState({ shouldShowModal: true });
+    this.setState({ modalImageSrc: props.target.src });
+    this.setState({ imageDescription: props.target.alt });
+
+    this.setState({ title: title });
+    console.log(title);
+  };
+
+  closeModal = () => {
+    this.setState({ shouldShowModal: false });
+    this.setState({ modalImageSrc: "" });
+    this.setState({ imageDescription: "" });
+    this.setState({ imageTitle: "" });
+  };
 
   handleClick = () => {
     this.setState({ favorites: this.state.favorites + 1 });
   };
   render() {
     return (
-      <Container fluid='md'>
-        <Row>
-          <div>
-            <Col lg={true}>
-              {jsonGallery.map((image, i) => (
-                <div className='beasts' key={i}>
-                  <img
-                    src={`${image.image_url}`}
-                    alt={image.description}
-                    key={i}
-                  />
-                  <li>
-                    {image.title}
-                    <br />
-                    {image.description}
-                    <br />
-                    Number of horns {image.horns}
-                    <br />
-                    <p>
-                      {" "}
-                      This beast has been favorited {
-                        this.state.favorites
-                      } times{" "}
-                    </p>
-                    <button onClick={this.handleClick}> Favorites</button>
-                  </li>
-                </div>
-              ))}
-            </Col>
-          </div>
-        </Row>
-      </Container>
+      <div>
+        <SelectedBeast
+          closeModal={this.closeModal}
+          shouldShowModal={this.state.shouldShowModal}
+          modalImageSrc={this.state.modalImageSrc}
+          imageDescription={this.state.imageDescription}
+          title={this.state.title}
+        />
+
+        <Card style={{ width: "18rem" }}>
+          {jsonGallery.map((image, i) => (
+            <div className='beasts' key={i}>
+              <Card.Img
+                variant='top'
+                src={image.image_url}
+                alt={image.description}
+                key={i}
+                onClick={this.openModal(image.title)}
+              />
+              <Card.Body>
+                <Card.Title>{image.title}</Card.Title>
+                {/* TODO: Add description */}
+                <Card.Text>{image.description} </Card.Text>
+                <Card.Text>Number of horns {image.horns}</Card.Text>
+                <Card.Text>
+                  This beast has been favorited {this.state.favorites}
+                </Card.Text>
+                <Button variant='primary' onClick={this.handleClick}>
+                  Favorite
+                </Button>
+              </Card.Body>
+            </div>
+          ))}
+        </Card>
+      </div>
     );
   }
 }
